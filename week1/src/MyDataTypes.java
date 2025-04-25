81736661ea6ef5f74d7dabc411e60a8904d22137
Debeshg <debeshdessie@gmail.com>
2025-04-25 11:00:18 -0700

    Add files via upload

---
+++ week1/src/MyDataTypes.java
@@ -0,0 +1,29 @@
+public class MyDataTypes {
+    public static void main (String[] args) {
+        short number1 = 32_000;
+        long number2 = 2_000_000_000L;
+        Float number3 = 30.58f;
+        int number4 = 2_000_000_000;
+        double number5 = 50.45637377;
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(number5);
+
+
+        Boolean dry = false;
+
+        if(dry) {
+
+
+            System.out.println("Time to hang up those curtains!!!");
+        }  else {
+            System.out.println("we will have to wait a spell");
+        }
+        char best = 'A';
+             System.out.println(++best);
+            }
+
+        }
